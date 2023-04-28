6c10e07b2c1461490a78e61917bf557bbe5740c7
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-04-28 18:32:39 +0530

    b3 express + mongo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,60 @@
-import { getAllUsers } from "./api.demo.js";
+import express from "express";
+const app = express();
 
-async function main() {
-  let result = await getAllUsers();
+import { MongoClient } from "mongodb";
 
-  console.log(result);
-}
+app.get("/", (req, res) => {
+  let message = { success: true };
+  res.json(message);
+});
 
-main();
+app.get("/insert1", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+
+  const client = new MongoClient(uri);
+  const usersColl = client.db("practice").collection("users");
+
+  let newDocument = { username: "delhi" };
+  let resultDoc = await usersColl.insertOne(newDocument);
+  console.log(resultDoc);
+  client.close();
+
+  let message = { success: true };
+  res.json(message);
+});
+
+app.get("/insert2", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+
+  const client = new MongoClient(uri);
+  const usersColl = client.db("practice").collection("users");
+
+  let username = req.query.username || "delhi";
+
+  let newDocument = { username: username };
+  let resultDoc = await usersColl.insertOne(newDocument);
+  console.log(resultDoc);
+  client.close();
+
+  let message = { success: true };
+  res.json(message);
+});
+
+app.get("/insert3", async (req, res) => {
+  const uri = "mongodb://localhost:27017";
+
+  const client = new MongoClient(uri);
+  const usersColl = client.db("practice").collection("users");
+
+  let username = req.query.username || "delhi";
+
+  let newDocument = { username: username };
+  let resultDoc = await usersColl.insertOne(newDocument);
+  console.log(resultDoc);
+  client.close();
+
+  let message = { success: true };
+  res.json(message);
+});
+
+app.listen(3000, () => console.log(`Example app listening on port 3000`));
