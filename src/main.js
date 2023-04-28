c4730b69cf991414fbbc03124efb24858dda338a
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-04-28 13:21:46 +0530

    baisc import export.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import { project, price, ref, hello } from "./app.config.js";
+import { sum } from "./calculator.js";
 
-console.log(project);
-console.log(price);
-console.log(ref);
+function main() {
+  console.log(project);
+  console.log(price);
+  console.log(ref);
 
-hello();
+  hello();
+
+  let output = sum(1, 1);
+  console.log(output);
+}
+
+main();
