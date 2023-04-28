de7ac3e731b14caf542ba57db24b54bfb421f7f2
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-04-28 18:41:46 +0530

    post.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { saveOneDocument } from "./appmongo/insert.util.js";
+
 const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/", (req, res) => {
   let message = { success: true };
@@ -12,6 +15,14 @@ app.get("/insert", async (req, res) => {
   let email = req.query.email || "default@gmail.com";
 
   let newDocument = { username, email };
+  await saveOneDocument(newDocument);
+
+  let message = { success: true };
+  res.json(message);
+});
+
+app.post("/insert", async (req, res) => {
+  let newDocument = req.body;
   await saveOneDocument(newDocument);
 
   let message = { success: true };
